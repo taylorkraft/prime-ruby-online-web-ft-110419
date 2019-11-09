bc9f61d336f9f46fd23c8cd10e82355a16fcbfee
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-09 16:11:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,2 @@
 def prime?(num)
-  if !num.integer?
-    return false
-    if num < 2 
-      return false 
-      if num == 2
-        return true 
-        if num (2..num-1)
-          return false
-        end
-      end
+  return false if num == int
