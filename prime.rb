3e290d484a8997cae40b4db2942e4c2eee76f618
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-09 16:14:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,4 +2,5 @@ def prime?(num)
   return false if !num.integer? 
   return false is num < 2
   return true if num == 2 
-  (2..num-1).each {|int| if num % int == 0 
+  (2..num-1).each {|int| return false if num % int == 0}
+end
